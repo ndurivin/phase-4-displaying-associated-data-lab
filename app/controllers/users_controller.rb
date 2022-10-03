10b7a7277de7262df8fe0ff30b2bb5db5daad365
Vinn Nduri <ndurivo@gmail.com>
2022-10-03 10:38:59 +0300

    Done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+    
+    def index
+        users = User.all
+        render json: users, status: :ok
+    end
+
+    def show
+        user = User.find_by(id: params[:id])
+        render json: user,  include: :items, status: :ok
+    end
 end
